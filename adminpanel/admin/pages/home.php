4171dbb77e0ea2633a4c23ca8686e47f2eabbca8
Obaje Josiah <drdrejessy@yahoo.com>
2021-06-03 17:56:37 +0100

    First commit Message

---
+++ adminpanel/admin/pages/home.php
@@ -0,0 +1,205 @@
+
+
+<div class="app-main__outer">
+    <div id="refreshData">
+    <div class="app-main__inner">
+            <div class="app-page-title">
+                <div class="page-title-wrapper">
+                    <div class="page-title-heading">
+                        <div class="page-title-icon">
+                            <i class="pe-7s-car icon-gradient bg-mean-fruit">
+                            </i>
+                        </div>
+                        <div>Students Analytics Dashboard
+                            <div class="page-title-subheading">
+                        </div>
+                    </div>
+                    <div class="page-title-actions">
+                        <button type="button" data-toggle="tooltip" title="Example Tooltip" data-placement="bottom" class="btn-shadow mr-3 btn btn-dark">
+                            <i class="fa fa-star"></i>
+                        </button>
+                        <div class="d-inline-block dropdown">
+                            <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn-shadow dropdown-toggle btn btn-info">
+                                <span class="btn-icon-wrapper pr-2 opacity-7">
+                                    <i class="fa fa-business-time fa-w-20"></i>
+                                </span>
+                                Buttons
+                            </button>
+                            <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
+                                <ul class="nav flex-column">
+                                    <li class="nav-item">
+                                        <a href="javascript:void(0);" class="nav-link">
+                                            <i class="nav-link-icon lnr-inbox"></i>
+                                            <span>
+                                                Inbox
+                                            </span>
+                                            <div class="ml-auto badge badge-pill badge-secondary">0</div>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="javascript:void(0);" class="nav-link">
+                                            <i class="nav-link-icon lnr-book"></i>
+                                            <span>
+                                                Book
+                                            </span>
+                                            <div class="ml-auto badge badge-pill badge-danger">0</div>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="javascript:void(0);" class="nav-link">
+                                            <i class="nav-link-icon lnr-picture"></i>
+                                            <span>
+                                                Picture
+                                            </span>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a disabled href="javascript:void(0);" class="nav-link disabled">
+                                            <i class="nav-link-icon lnr-file-empty"></i>
+                                            <span>
+                                                File Disabled
+                                            </span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>   
+                 </div>
+            </div>            <div class="row">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-midnight-bloom">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Subject</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selCourse['totCourse']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-arielle-smile">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Exam</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white">
+                                    <span><?php echo $totalCourse = $selExam['totExam']; ?></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-grow-early">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Total Examinee</div>
+                                <div class="widget-subheading" style="color:transparent;">.</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-white"><span>100%</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content bg-premium-dark">
+                        <div class="widget-content-wrapper text-white">
+                            <div class="widget-content-left">
+                                <div class="widget-heading">Students</div>
+                                <div class="widget-subheading">Registered Students</div>
+                            </div>
+                            <div class="widget-content-right">
+                                <div class="widget-numbers text-warning"><span>500 Students</span></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="row">
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Students</div>
+                                    <div class="widget-subheading">Last year Registered Students</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-success">350</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Past Examination</div>
+                                    <div class="widget-subheading">Strenght</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-warning">69%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Followers</div>
+                                    <div class="widget-subheading">Parents Interest</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-danger">55.9%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-xl-none d-lg-block col-md-6 col-xl-4">
+                    <div class="card mb-3 widget-content">
+                        <div class="widget-content-outer">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left">
+                                    <div class="widget-heading">Parents Interest</div>
+                                    <div class="widget-subheading">Expected Interest 2021 Entrance Intake</div>
+                                </div>
+                                <div class="widget-content-right">
+                                    <div class="widget-numbers text-focus">89.2%</div>
+                                </div>
+                            </div>
+                            <div class="widget-progress-wrapper">
+                                <div class="progress-bar-sm progress-bar-animated-alt progress">
+                                    <div class="progress-bar bg-info" role="progressbar" aria-valuenow="54" aria-valuemin="0" aria-valuemax="100" style="width: 54%;"></div>
+                                </div>
+                                <div class="progress-sub-label">
+                                    <div class="sub-label-left">Expected Inconvinient Rate</div>
+                                    <div class="sub-label-right">21.9%</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- <?php include("includes/graph.php"); ?> -->
+      
+        
+        </div>
+         
+    </div>
